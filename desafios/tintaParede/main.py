0f73b266a913b160f3440ca8ae3908806ffd35ce
Thiago Ribeiro Queiroz <thigaswork@gmail.com>
2025-09-19 22:18:12 -0300

    latas de tinta desafio

---
+++ desafios/tintaParede/main.py
@@ -0,0 +1,12 @@
+rendimento = int(input('Qual o rendimento da lata? '))
+altura = int(input('Qual a altura da parede? '))
+largura = int(input('Qual a largura da parede? '))
+
+def calculo_tinta():
+    area = altura * largura
+    total = area/rendimento
+    print(f'Você precisa de {total} latas de tinta')
+
+calculo_tinta()
+
+     
